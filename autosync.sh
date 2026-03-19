f7a3cc6654fb65b28b3193dd31d7284e93608756
Himanshu04-stack <himanshu0004garg@gmail.com>
2026-03-19 18:30:53 +0530

    Auto sync: Thu Mar 19 18:30:53 IST 2026

--- autosync.sh
+++ autosync.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-git add .
-git commit -m "Auto sync: $(date)"
-git push
+git pull origin main --rebase
+
+if [[ -n $(git status -s) ]]; then
+  git add .
+  git commit -m "Auto sync: $(date)"
+  git push
+else
+  echo "No changes to sync"
+fi
